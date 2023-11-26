d9822ad0de359b4b10e043ce844487b79754aab5
The-Anh Vu-Le <vltanh@illinois.edu>
2023-11-26 13:53:14 -0600

    add brute-force marginal and conditional distribution computation

--- brute_force.py
+++ brute_force.py
@@ -6,7 +6,7 @@ from energy_based import distribution
 from configurations import load_configurations
 
 
-def marginal_rbm_bf(W, y):
+def marginal_v_rbm_bf(W, y):
     '''
     Compute the marginal distribution of the visible states
 
@@ -24,12 +24,235 @@ def marginal_rbm_bf(W, y):
     x = load_configurations(n).type(DEFAULT_TYPE)  # [n, 2^n]
 
     # Compute the distribution
-    p = distribution(W, y, x, energy_rbm)  # [B, ]
+    p = distribution(W, y, x, energy_rbm)  # [B, 2^n]
 
     # Compute the marginal distribution
     p = p.reshape(-1, 2 ** n_v, 2 ** n_h)  # [B, 2^n_v, 2^n_h]
     p = torch.sum(p, dim=2)  # [B, 2^n_v]
     return p
+
+
+def marginal_h_rbm_bf(W, y):
+    '''
+    Compute the marginal distribution of the hidden states
+
+    Args:
+        W: [n_h, n_v]
+        y: [B, n]
+
+    Returns:
+        p: [B, 2^n_h]
+    '''
+    n_h, n_v = W.shape
+    n = n_v + n_h
+
+    # Generate all configurations
+    x = load_configurations(n).type(DEFAULT_TYPE)  # [n, 2^n]
+
+    # Compute the distribution
+    p = distribution(W, y, x, energy_rbm)  # [B, 2^n]
+
+    # Compute the marginal distribution
+    p = p.reshape(-1, 2 ** n_v, 2 ** n_h)  # [B, 2^n_v, 2^n_h]
+
+    p = torch.sum(p, dim=1)  # [B, 2^n_h]
+    return p
+
+
+def barycenter_v_rbm_bf(W, y):
+    '''
+    Compute the barycenter of the visible states
+
+    Args:
+        W: [n_h, n_v]
+        y: [B, n]
+
+    Returns:
+        barycenter: [B, n_v]
+    '''
+    n_h, n_v = W.shape
+    n = n_v + n_h
+
+    # Compute the marginal distribution
+    p = marginal_v_rbm_bf(W, y)  # [B, 2^n_v]
+
+    # Generate all configurations
+    x = load_configurations(n).type(DEFAULT_TYPE)  # [n, 2^n]
+
+    # Compute the barycenter
+    x = x[:n_v, ::2**n_h]  # [n_v, 2^n_v]
+
+    # Compute the barycenter
+    # [B, 2^n_v] @ [2^n_v, n_v] = [B, n_v]
+    barycenter = torch.matmul(p, x.transpose(0, 1))  # [B, n_v]
+
+    return barycenter
+
+
+def barycenter_h_rbm_bf(W, y):
+    '''
+    Compute the barycenter of the hidden states
+
+    Args:
+        W: [n_h, n_v]
+        y: [B, n]
+
+    Returns:
+        barycenter: [B, n_h]
+    '''
+    n_h, n_v = W.shape
+    n = n_v + n_h
+
+    # Compute the marginal distribution
+    p = marginal_h_rbm_bf(W, y)  # [B, 2^n_h]
+
+    # Generate all configurations
+    x = load_configurations(n).type(DEFAULT_TYPE)  # [n, 2^n]
+
+    # Compute the barycenter
+    x = x[n_v:, :2**n_h]  # [n_v, 2^n_v]
+
+    # Compute the barycenter
+    # [B, 2^n_v] @ [2^n_v, n_v] = [B, n_v]
+    barycenter = torch.matmul(p, x.transpose(0, 1))  # [B, n_v]
+
+    return barycenter
+
+
+def conditional_h_rbm_bf(W, y, h):
+    '''
+    Compute the conditional distribution p(v | h)
+
+    Args:
+        W: [n_h, n_v]
+        y: [B, n]
+        h: [B, n_h]
+
+    Returns:
+        p: [B, 2^n_v]
+    '''
+    n_h, n_v = W.shape
+    n = n_v + n_h
+    B = h.shape[0]
+
+    # Generate all configurations
+    x = load_configurations(n).type(DEFAULT_TYPE)  # [n, 2^n]
+
+    # Compute the distribution
+    p = distribution(W, y, x, energy_rbm)  # [B, 2^n]
+
+    # Extract the configurations that satisfy the condition
+    x_h = x[n_v:, :]  # [n_h, 2^n]
+    # [B, n_h, 1] - [1, n_h, 2^n]
+    diff = h[:, :, None] - x_h[None, :, :]  # [B, n_h, 2^n]
+    mask = (diff == 0).all(dim=1)  # [B, 2^n]
+
+    # Compute the conditional distribution
+    p = p[mask].reshape(B, -1)  # [B, 2^n_v]
+    p = p / torch.sum(p, dim=1, keepdim=True)  # [B, 2^n_v]
+
+    return p
+
+
+def conditional_v_rbm_bf(W, y, v):
+    '''
+    Compute the conditional distribution p(h | v)
+
+    Args:
+        W: [n_h, n_v]
+        y: [B, n]
+        v: [B, n_v]
+
+    Returns:
+        p: [B, 2^n_h]
+    '''
+    n_h, n_v = W.shape
+    n = n_v + n_h
+    B = v.shape[0]
+
+    # Generate all configurations
+    x = load_configurations(n).type(DEFAULT_TYPE)  # [n, 2^n]
+
+    # Compute the distribution
+    p = distribution(W, y, x, energy_rbm)  # [B, 2^n]
+
+    # Extract the configurations that satisfy the condition
+    x_v = x[:n_v, :]  # [n_v, 2^n]
+    # [B, n_v, 1] - [1, n_v, 2^n]
+    diff = v[:, :, None] - x_v[None, :, :]  # [B, n_v, 2^n]
+    mask = (diff == 0).all(dim=1)  # [B, 2^n]
+
+    # Compute the conditional distribution
+    p = p[mask].reshape(B, -1)  # [B, 2^n_h]
+    p = p / torch.sum(p, dim=1, keepdim=True)  # [B, 2^n_h]
+
+    return p
+
+
+def barycenter_conditional_h_rbm_bf(W, y, h):
+    '''
+    Compute the barycenter of the conditional distribution p(v | h)
+
+    Args:
+        W: [n_h, n_v]
+        y: [B, n]
+        h: [B, n_h]
+
+    Returns:
+        m: [B, n_v]
+    '''
+    n_h, n_v = W.shape
+    n = n_v + n_h
+
+    # Compute the conditional distribution
+    p = conditional_h_rbm_bf(W, y, h)  # [B, 2^n_v]
+
+    # Generate all configurations
+    x = load_configurations(n).type(DEFAULT_TYPE)  # [n, 2^n]
+
+    # Extract the configurations that satisfy the condition
+    x_h = x[n_v:, :]  # [n_h, 2^n]
+    # [B, n_h, 1] - [1, n_h, 2^n] = [B, n_h, 2^n]
+    diff = h[:, :, None] - x_h[None, :, :]
+    mask = (diff == 0).all(dim=1)  # [B, 2^n]
+    x_v = x[:n_v, mask[0]]  # [n_v, 2^n_v]
+
+    # Compute the barycenter
+    m = torch.matmul(p, x_v.transpose(0, 1))  # [B, n_v]
+    return m
+
+
+def barycenter_conditional_v_rbm_bf(W, y, v):
+    '''
+    Compute the barycenter of the conditional distribution p(h | v)
+
+    Args:
+        W: [n_h, n_v]
+        y: [B, n]
+        v: [B, n_v]
+
+    Returns:
+        m: [B, n_h]
+    '''
+    n_h, n_v = W.shape
+    n = n_v + n_h
+
+    # Compute the conditional distribution
+    p = conditional_v_rbm_bf(W, y, v)  # [B, 2^n_h]
+
+    # Generate all configurations
+    x = load_configurations(n).type(DEFAULT_TYPE)  # [n, 2^n]
+
+    # Extract the configurations that satisfy the condition
+    x_v = x[:n_v, :]  # [n_v, 2^n]
+    # [B, n_v, 1] - [1, n_v, 2^n]
+    diff = v[:, :, None] - x_v[None, :, :]  # [B, n_v, 2^n]
+    mask = (diff == 0).all(dim=1)  # [B, 2^n]
+    x_h = x[n_v:, mask[0]]  # [n_h, 2^n_h]
+
+    # Compute the barycenter
+    m = torch.matmul(p, x_h.transpose(0, 1))  # [B, n_v]
+    return m
 
 
 def barycenter_bf(A, y, energy_fn):
---
+++ test_conditional.py
@@ -0,0 +1,67 @@
+import torch
+
+from constant import DEFAULT_TYPE, SEED
+from brute_force import marginal_h_rbm_bf, marginal_v_rbm_bf, \
+    conditional_h_rbm_bf, conditional_v_rbm_bf, \
+    barycenter_conditional_h_rbm_bf, barycenter_conditional_v_rbm_bf
+
+# Set default float precision
+torch.set_default_dtype(DEFAULT_TYPE)
+
+# Set seed to be consistent
+torch.manual_seed(SEED)
+
+
+def main():
+    # Set dimensions
+    n_v, n_h = 5, 5
+    n = n_v + n_h
+    B = 1
+
+    # Generate a random RBM
+    W = torch.randn(n_h, n_v).type(DEFAULT_TYPE)
+    y = torch.randn(B, n)
+    # y = torch.zeros(B, n)
+
+    print('W\n', W)
+    print('y\n', y)
+
+    # === Fix h ===
+    print('=========')
+    h = torch.randint(0, 2, (B, n_h)).type(DEFAULT_TYPE)
+    h = 2 * h - 1
+    print('h\n', h)
+
+    # Compute the marginal distribution p(h)
+    p_h = marginal_h_rbm_bf(W, y)
+    print('p(h)\n', p_h)
+
+    # Compute the conditional distribution
+    p_v_given_h = conditional_h_rbm_bf(W, y, h)
+    print('p(v|h)\n', p_v_given_h)
+
+    # Compute the barycenter of the conditional distribution
+    E_v_given_h = barycenter_conditional_h_rbm_bf(W, y, h)
+    print('E(v|h)\n', E_v_given_h)
+
+    # === Fix v ===
+    print('=========')
+    v = torch.randint(0, 2, (B, n_v)).type(DEFAULT_TYPE)
+    v = 2 * v - 1
+    print('v\n', v)
+
+    # Compute the marginal distribution p(v)
+    p_v = marginal_v_rbm_bf(W, y)
+    print('p(v)\n', p_v)
+
+    # Compute the conditional distribution
+    p_h_given_v = conditional_v_rbm_bf(W, y, v)
+    print('p(h|v)\n', p_h_given_v)
+
+    # Compute the barycenter of the conditional distribution
+    E_h_given_v = barycenter_conditional_v_rbm_bf(W, y, v)
+    print('E(h|v)\n', E_h_given_v)
+
+
+if __name__ == '__main__':
+    main()
